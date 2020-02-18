da5545b96fb922f5c012d2cbcc75aa842e534c82
motabass <markus.mohoritsch@gmx.at>
2020-02-18 18:29:43 +0100

    storybook init für slide-panel

--- libs/ui-components/slide-panel/src/lib/slide-panel.component.stories.ts
+++ libs/ui-components/slide-panel/src/lib/slide-panel.component.stories.ts
@@ -1,15 +1,27 @@
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { SlidePanelComponent } from './slide-panel.component';
 
 export default {
   title: 'SlidePanelComponent'
-}
+};
 
-export const primary = () => ({
+export const open = () => ({
   moduleMetadata: {
-    imports: []
+    imports: [MatIconModule, MatButtonModule]
   },
   component: SlidePanelComponent,
   props: {
+    opened: true
   }
-})
+});
+
+export const closed = () => ({
+  moduleMetadata: {
+    imports: [MatIconModule, MatButtonModule]
+  },
+  component: SlidePanelComponent,
+  props: {
+    opened: false
+  }
+});
--- libs/ui-components/slide-panel/src/lib/slide-panel.component.ts
+++ libs/ui-components/slide-panel/src/lib/slide-panel.component.ts
@@ -1,17 +1,24 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'mtb-slide-panel',
   templateUrl: './slide-panel.component.html',
   styleUrls: ['./slide-panel.component.scss']
 })
-export class SlidePanelComponent implements OnInit {
+export class SlidePanelComponent implements OnChanges {
+  @Input()
   opened = true;
 
-  constructor(private host: ElementRef, private renderer: Renderer2) {
-  }
+  constructor(private host: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if (this.opened === true) {
+      this.renderer.removeClass(this.host.nativeElement, 'closed-slide-panel');
+      this.renderer.addClass(this.host.nativeElement, 'opened-slide-panel');
+    } else {
+      this.renderer.removeClass(this.host.nativeElement, 'opened-slide-panel');
+      this.renderer.addClass(this.host.nativeElement, 'closed-slide-panel');
+    }
   }
 
   open() {
