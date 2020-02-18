ae96e1263478f24a1c368acf91211c7dfe7d7439
motabass <markus.mohoritsch@gmx.at>
2020-02-18 01:38:30 +0100

    pwa init

--- apps/hsr/src/app/app.module.ts
+++ apps/hsr/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { PlayerModule } from '@motabass/ui-components/player';
 import { SlidePanelModule } from '@motabass/ui-components/slide-panel';
 import { VisualsModule } from '@motabass/ui-components/visuals';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, PlayerModule, SlidePanelModule, VisualsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    PlayerModule,
+    SlidePanelModule,
+    VisualsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
