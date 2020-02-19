920bb594af69c88214cb88b9e30fe57ad441e6c9
motabass <markus.mohoritsch@gmx.at>
2020-02-19 14:26:13 +0100

    fixed button

--- libs/ui-components/dialogs/src/lib/dialogs.module.ts
+++ libs/ui-components/dialogs/src/lib/dialogs.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PromptDialogComponent } from './prompt-dialog/prompt-dialog.component';
 
 @NgModule({
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   declarations: [PromptDialogComponent],
   exports: [PromptDialogComponent]
 })
