8f55b8b4593842603ec12620e9d617f301cbca05
motabass <markus.mohoritsch@gmx.at>
2020-02-28 01:58:44 +0100

    electron backend app added

---
+++ apps/motabass-desktop/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'motabass-desktop',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/motabass-desktop'
+};
---
+++ apps/motabass-desktop/src/app/constants.ts
@@ -0,0 +1,3 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'motabass';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
