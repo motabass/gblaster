96279321d83d56332b5f8f637b4f467eb4f8ca3c
motabass <markus.mohoritsch@gmx.at>
2020-03-24 00:25:01 +0100

    prettier 2.0 formatted

--- tools/electron/postinstall.js
+++ tools/electron/postinstall.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const f_angular = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
-fs.readFile(f_angular, 'utf8', function(err, data) {
+fs.readFile(f_angular, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
@@ -11,7 +11,7 @@ fs.readFile(f_angular, 'utf8', function(err, data) {
   var result = result.replace(/target: "web",/g, '');
   var result = result.replace(/return \{/g, 'return {target: "electron-renderer",');
 
-  fs.writeFile(f_angular, result, 'utf8', function(err) {
+  fs.writeFile(f_angular, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
