572a9f878e8d4f1a73fb9f9952d4abdac8ec29c1
motabass <markus.mohoritsch@gmx.at>
2020-03-30 00:25:03 +0200

    release v0.20.0

--- libs/player/src/lib/metadata-service/lastfm-metadata.service.ts
+++ libs/player/src/lib/metadata-service/lastfm-metadata.service.ts
@@ -5,14 +5,14 @@ import { RemoteCoverPicture } from './metadata.types';
 
 @Injectable({ providedIn: 'any' })
 export class LastfmMetadataService {
-  readonly #LASTFM_API_KEY = '3a67934408152a2fc3f7216c022ec1df';
+  private readonly LASTFM_API_KEY = '3a67934408152a2fc3f7216c022ec1df';
 
   constructor(private http: HttpClient) {}
 
   async getCoverPicture(tags: Id3Tags): Promise<RemoteCoverPicture | undefined> {
     if (tags.artist && tags.album) {
       const data: any = await this.http
-        .get(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${this.#LASTFM_API_KEY}&artist=${tags.artist}&album=${tags.album}&format=json`)
+        .get(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${this.LASTFM_API_KEY}&artist=${tags.artist}&album=${tags.album}&format=json`)
         .toPromise();
       console.log(data);
 
