3e8e5d0b2b34ed87848acbb621f7fced592d1055
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2020-08-19 22:25:48 +0200

    deps and polyfills removed

--- apps/gblaster/src/polyfills.ts
+++ apps/gblaster/src/polyfills.ts
@@ -64,17 +64,3 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-// (window as any).global = window;
-// global.Buffer = global.Buffer || require('buffer').Buffer;
-// (window as any).process = {
-//   version: ''
-// };
-
-import * as process from 'process';
-(window as any).process = process;
-
-(window as any).global = window;
-
-import * as _buffer from 'buffer';
-(window as any).Buffer = _buffer.Buffer; // note: the trailing slash is important!
