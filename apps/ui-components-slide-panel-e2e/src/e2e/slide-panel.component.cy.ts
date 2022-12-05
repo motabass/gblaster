9d81efd89bb3e6b298743400fa5015ff001fc9c0
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2022-12-05 02:16:51 +0100

    removed storybook

--- apps/ui-components-slide-panel-e2e/src/e2e/slide-panel.component.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('ui-components-slide-panel', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=slidepanelcomponent--primary'));
-
-  it('should render the component', () => {
-    cy.get('mtb-slide-panel').should('exist');
-  });
-});
