33d73d9f5eea7092c6a7db3f700737355bdd0934
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2023-01-07 02:39:57 +0100

    fixed e2e

--- apps/gblaster-e2e/src/support/app.po.ts
+++ apps/gblaster-e2e/src/support/app.po.ts
@@ -1,3 +1,3 @@
 export const getToolBarTitle = () => cy.get('.title');
 export const getSidenavToggleButton = () => cy.get('#sidenav_menu_button');
-export const getSidenavLink = (link: number) => cy.get(`:nth-child(${link}) > .mat-list-item-content`);
+export const getSidenavLink = (link: number) => cy.get(`:nth-child(${link}) > .mdc-list-item__content`);
