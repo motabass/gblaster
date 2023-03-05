d9677d0dfedf96c48d1c1d4672408b68e3660f5b
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2023-03-05 12:28:51 +0100

    Release 13.2.2

--- libs/ui-components/slide-panel/jest.config.ts
+++ libs/ui-components/slide-panel/jest.config.ts
@@ -4,13 +4,7 @@ export default {
   coverageDirectory: '../../../coverage/libs/ui-components/slide-panel',
 
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      stringifyContentPathRegex: '\\.(html|svg)$',
-
-      tsconfig: '<rootDir>/tsconfig.spec.json'
-    }
-  },
+  globals: {},
   displayName: 'ui-components-slide-panel',
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
@@ -18,7 +12,14 @@ export default {
     'jest-preset-angular/build/serializers/html-comment'
   ],
   transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        stringifyContentPathRegex: '\\.(html|svg)$',
+
+        tsconfig: '<rootDir>/tsconfig.spec.json'
+      }
+    ]
   },
   transformIgnorePatterns: ['node_modules/(?!.*.mjs$)']
 };
