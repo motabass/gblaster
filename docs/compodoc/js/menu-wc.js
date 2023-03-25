42a58d9c0ab2bba5556111355b9a4f3650d4f361
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2023-03-25 09:41:05 +0100

    Release 13.3.1

--- docs/compodoc/js/menu-wc.js
+++ docs/compodoc/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">motabass</a>
+                    <a href="index.html" data-type="index-link">gblaster</a>
                 </li>
 
                 <li class="divider"></li>
