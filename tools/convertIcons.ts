6bd60c58921d09bfff6718c682042375651d1498
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2023-12-08 07:08:00 +0100

    optimized icon set converter

--- tools/convertIcons.ts
+++ tools/convertIcons.ts
@@ -3,41 +3,51 @@ import * as path from 'path';
 import { optimize, loadConfig } from 'svgo';
 
 const optimizeSvg = async (svg: string): Promise<string> => {
-  const config = await loadConfig('tools/svgo.config.js');
-  const optimized = await optimize(svg, config);
-  return optimized.data;
+  try {
+    const config = await loadConfig('tools/svgo.config.js');
+    const optimized = await optimize(svg, config);
+    return optimized.data;
+  } catch (err) {
+    console.error('Error optimizing SVG:', err);
+    throw err;
+  }
 };
 
 const convertSvgFileToSvgIconMapString = async (svgIconFile: string, svgFilesFolder: string): Promise<string> => {
   let fileContent = await fs.promises.readFile(path.join(svgFilesFolder, svgIconFile), 'utf8');
   fileContent = await optimizeSvg(fileContent);
-  console.log(fileContent);
   const fileId = svgIconFile.replace('.svg', '');
-  fileContent = fileContent.replace('<svg ', '<svg id="' + fileId + '" ');
+  fileContent = fileContent.replace('<svg ', `<svg id="${fileId}" `);
   return fileContent;
 };
 
 const convertToIconSet = async (svgFilesFolder: string, svgMapFile: string): Promise<void> => {
   let files: string[] = [];
 
-  console.log(path.join(svgFilesFolder));
-
   try {
     files = await fs.promises.readdir(path.join(svgFilesFolder));
   } catch (err) {
-    return console.log('Unable to scan directory: ' + err);
+    console.error('Unable to scan directory:', err);
+    throw err;
   }
 
-  let iconSetSvgString = '';
+  const iconSetSvgStrings = [];
 
   for (const svgIconFile of files) {
     const fileContent = await convertSvgFileToSvgIconMapString(svgIconFile, svgFilesFolder);
-    iconSetSvgString = iconSetSvgString + fileContent;
+    iconSetSvgStrings.push(fileContent);
   }
 
-  const finalIconSetSvgString = '<svg><defs>' + iconSetSvgString + '</defs></svg>';
+  const finalIconSetSvgString = `<svg><defs>${iconSetSvgStrings.join('')}</defs></svg>`;
 
-  fs.writeFileSync(path.join(svgMapFile), finalIconSetSvgString, { encoding: 'utf8' });
+  try {
+    fs.writeFileSync(path.join(svgMapFile), finalIconSetSvgString, { encoding: 'utf8' });
+  } catch (err) {
+    console.error('Error writing SVG file:', err);
+    throw err;
+  }
 };
 
-convertToIconSet('libs/icons', 'apps/gblaster/src/assets/icon-set.svg').then(() => console.log('Icon-Set successful generated'));
+convertToIconSet('libs/icons', 'apps/gblaster/src/assets/icon-set.svg')
+  .then(() => console.log('Icon-Set successful generated'))
+  .catch((err) => console.error('Error generating icon set:', err));
