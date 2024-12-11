0a9aedcbe38eb0df46eb6b531cfcb29996b815d7
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2024-12-11 01:32:40 +0100

    chore: updated config to latest nx defaults

--- apps/gblaster/jest.config.ts
+++ apps/gblaster/jest.config.ts
@@ -1,22 +1,21 @@
 export default {
+  displayName: 'gblaster',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {},
-  displayName: 'gblaster',
+  coverageDirectory: '../../coverage/apps/gblaster',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment'
-  ],
-  transform: {
-    '^.+.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        stringifyContentPathRegex: '\\.(html|svg)$',
-
-        tsconfig: '<rootDir>/tsconfig.spec.json'
-      }
-    ]
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)']
+  ]
 };
