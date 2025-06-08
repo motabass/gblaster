d3c2f7e7b9ac0720ed348d8216e812ac20bff318
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2025-06-08 09:24:20 +0200

    Release 20.3.0

--- docs/static/styles.js
+++ docs/static/styles.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[869],{4941:()=>{}},s=>{var e;e=4941,s(s.s=e)}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[869],{932:()=>{}},s=>{var e;e=932,s(s.s=e)}]);
