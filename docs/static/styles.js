0ac8d559fdc3a2216b142b30dc8e378a30590ddf
Markus Mohoritsch <markus.mohoritsch@gmx.at>
2026-02-18 01:12:50 +0100

    chore(release): 2026.2.0

--- docs/static/styles.js
+++ docs/static/styles.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk_nx_graph_client=self.webpackChunk_nx_graph_client||[]).push([[869],{5376:()=>{}},e=>{var n;n=5376,e(e.s=n)}]);
+"use strict";(self.webpackChunk_nx_graph_client=self.webpackChunk_nx_graph_client||[]).push([[869],{8721:()=>{}},e=>{var n;n=8721,e(e.s=n)}]);
